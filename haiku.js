b374331060c788c208ea0f6f547d79a54d75899e
Jonathan D. Lettvin <jlettvin@gmail.com>
2016-04-27 19:14:28 -0400

    debug

--- haiku.js
+++ haiku.js
@@ -1,43 +1,332 @@
-//(function() {
-document.haiku = {speaking:'English'};
-function display(language) {
-  // This function copies a hidden haiku to the display table cell.
-  var source = document.getElementById(language);
-  var target = document.getElementById('HAIKU_TXT');
-  target.innerHTML = source.innerHTML.trim() || "Translation needed.";
-}
+function HAIKU(f) { return f.toString().split('\n').slice(1,-1).join('\n'); }
 
-// These functions handle mouse hover display changes.  CSS handles bgcolor.
-function mouseEnter(language) {                  display(language); }
-function mouseLeave(language) {   display(document.haiku.speaking); }
-function mouseClick(language) { document.haiku.speaking = language; }
-
-var haiku = $('.haiku');        // Fetch list of all hidden text
-var languages = haiku.length;   // Count the languages
-var width = 6;                  // Specify horizontal language count
-
-// Generate the menu on-the-fly from language names (id value of haiku)
-var table = '<table align="center">';
-for (var row of _.range(0, languages, width)) {
-  // Make a new table row
-  table += '<tr>';
-  for (var col of _.range(width)) {
-    var I = row + col;
-    var language = I;
-    if (language >= languages) break;  // Quit if no more languages.
-    var element = haiku[language];
-    var id = element.id;
-    var onEnter = ' onmouseenter="mouseEnter(\'' + id + '\')"';
-    var onLeave = ' onmouseleave="mouseLeave(\'' + id + '\')"';
-    var onClick =      ' onclick="mouseClick(\'' + id + '\')"';
-    var onMouse = onEnter + onLeave + onClick;
-    table += '<td class="Language" ' + onMouse + '>' + id + '</td>';
-  }
-  table += '</tr>';
-}
-table += '</table>';
-
-$('.MENU').html(table);         // Fill the menu with the generated table
-
-display(document.haiku.speaking); // Initialize display for default language.
-//})();
+document.haiku = {
+"Afrikaans": HAIKU(function() {/*
+Te oordeel sonde is sonde.
+God oordeel ons gou genoeg nie.
+Kom ons wees vriende tot dan.
+*/}),
+ "Albanian": HAIKU(function() {/*
+Duke gjykuar mëkatin është mëkat.
+Perëndia na gjykon mjaft shpejt.
+Le të jenë miqtë deri atëherë.
+*/}),
+ "Amharic": HAIKU(function() {/*
+ኃጢአት ላይ ከመፍረድ ኃጢአት ነው.
+አምላክ በቅርቡ በቂ ይፈርዳል.
+የአምላክ በዚያን ጊዜ ድረስ ጓደኞች እንሁን.
+*/}),
+ "Arabic": HAIKU(function() {/*
+الحكم على الخطيئة هي الخطيئة.
+الله يحكم لنا قريبا بما فيه الكفاية.
+دعنا نكون أصدقاء حتى ذلك الحين.
+*/}),
+ "Armenian": HAIKU(function() {/*
+Դատելով մեղքը մեղք է:
+Աստված դատում մեզ շուտով բավարար.
+Եկեք լինի Ընկերներ մինչ այդ:
+*/}),
+ "Azerbaijani": HAIKU(function() {/*
+günah Münsiflər günahdır.
+Allah bizə tezliklə kifayət qədər hökm.
+ardından qədər dost olsun.
+*/}),
+ "Basque": HAIKU(function() {/*
+sin ikusita sin da.
+Jainkoak epaitu digu bezain azkar.
+Izan gaitezen lagun ordura arte.
+*/}),
+ "Belarusian": HAIKU(function() {/*
+Калі судзіць грэх з'яўляецца грахом.
+Бог судзіць нас дастаткова хутка.
+Давайце будзем сябрамі, пакуль пазней.
+*/}),
+ "Bengali": HAIKU(function() {/*
+পাপ বিচার পাপ নেই.
+ঈশ্বর আমাদের শীঘ্রই পর্যাপ্ত ফয়সালা করেন.
+আমাদের তখন পর্যন্ত বন্ধুদের হউক.
+*/}),
+ "Bosnian": HAIKU(function() {/*
+potreban prevod.
+*/}),
+ "Bulgarian": HAIKU(function() {/*
+Превод необходимо.
+*/}),
+ "Catalan": HAIKU(function() {/*
+La traducció necessària.
+*/}),
+ "Cebuano": HAIKU(function() {/*
+gikinahanglan sa paghubad.
+*/}),
+ "Chichewa": HAIKU(function() {/*
+Translation zofunika.
+*/}),
+ "Chinese": HAIKU(function() {/*
+翻譯需要。
+*/}),
+ "Corsican": HAIKU(function() {/*
+Traduzione bisognu.
+*/}),
+ "Croatian": HAIKU(function() {/*
+Prevođenje potrebno.
+*/}),
+ "Czech": HAIKU(function() {/*
+Překlad potřeba.
+*/}),
+ "Danish": HAIKU(function() {/*
+Oversættelse nødvendig.
+*/}),
+ "Dutch": HAIKU(function() {/*
+Vertaling nodig.
+*/}),
+ "English": HAIKU(function() {/*
+'Tis sin to judge sin.
+God sorts us out soon enough.
+Let's be friends 'til then.
+*/}),
+ "Esperanto": HAIKU(function() {/*
+Translation bezonis.
+*/}),
+ "Estonian": HAIKU(function() {/*
+Tõlge vaja.
+*/}),
+ "Filipino": HAIKU(function() {/*
+kailangan Translation.
+*/}),
+ "Finnish": HAIKU(function() {/*
+Käännös tarvitaan.
+*/}),
+ "French": HAIKU(function() {/*
+Traduction nécessaire.
+*/}),
+ "Frisian": HAIKU(function() {/*
+Oersetting nedich.
+*/}),
+ "Galician": HAIKU(function() {/*
+Tradución necesario.
+*/}),
+ "Georgian": HAIKU(function() {/*
+თარგმანი საჭირო.
+*/}),
+ "German": HAIKU(function() {/*
+Übersetzung benötigt.
+*/}),
+ "Greek": HAIKU(function() {/*
+χρειάζεται μετάφραση.
+*/}),
+ "Gujarati": HAIKU(function() {/*
+અનુવાદ જરૂર છે.
+*/}),
+ "Haitian Creole": HAIKU(function() {/*
+Tradiksyon nesesè.
+*/}),
+ "Hausa": HAIKU(function() {/*
+Translation da ake bukata.
+*/}),
+ "Hawaiian": HAIKU(function() {/*
+Translation pono.
+*/}),
+ "Hebrew": HAIKU(function() {/*
+תרגום הצורך.
+*/}),
+ "Hindi": HAIKU(function() {/*
+अनुवाद की जरूरत है।
+*/}),
+ "Hmong": HAIKU(function() {/*
+Neeg txhais lus yuav tsum tau.
+*/}),
+ "Hungarian": HAIKU(function() {/*
+Fordítás szükséges.
+*/}),
+ "Icelandic": HAIKU(function() {/*
+Þýðing þörf.
+*/}),
+ "Igbo": HAIKU(function() {/*
+Translation mkpa.
+*/}),
+ "Indonesian": HAIKU(function() {/*
+Terjemahan diperlukan.
+*/}),
+ "Irish": HAIKU(function() {/*
+Aistriúchán teastáil.
+*/}),
+ "Italian": HAIKU(function() {/*
+Traduzione necessario.
+*/}),
+ "Japanese": HAIKU(function() {/*
+翻訳が必要。
+*/}),
+ "Javanese": HAIKU(function() {/*
+*/}),
+ "Kannada": HAIKU(function() {/*
+ಅನುವಾದ ಅಗತ್ಯ.
+*/}),
+ "Kazakh": HAIKU(function() {/*
+Аударма қажет.
+*/}),
+ "Khmer": HAIKU(function() {/*
+ការបកប្រែត្រូវការជាចាំបាច់។
+*/}),
+ "Korean": HAIKU(function() {/*
+번역이 필요합니다.
+*/}),
+ "Kurdish (Kurmanji)": HAIKU(function() {/*
+Werger pêwîst.
+*/}),
+ "Kyrgyz": HAIKU(function() {/*
+Котормо керек.
+*/}),
+ "Lao": HAIKU(function() {/*
+ການແປພາສາທີ່ຈໍາເປັນ.
+*/}),
+ "Latin": HAIKU(function() {/*
+*/}),
+ "Latvian": HAIKU(function() {/*
+Tulkošana nepieciešams.
+*/}),
+ "Lithuanian": HAIKU(function() {/*
+Vertimas reikia.
+*/}),
+ "Lexembourgish": HAIKU(function() {/*
+Iwwersetzung waren.
+*/}),
+ "Macedonian": HAIKU(function() {/*
+Превод е потребно.
+*/}),
+ "Malagasy": HAIKU(function() {/*
+Translation ilaina.
+*/}),
+ "Malay": HAIKU(function() {/*
+Terjemahan diperlukan.
+*/}),
+ "Malayalam": HAIKU(function() {/*
+പരിഭാഷ ആവശ്യമായിരുന്നു.
+*/}),
+ "Maltese": HAIKU(function() {/*
+Traduzzjoni meħtieġa.
+*/}),
+ "Maori": HAIKU(function() {/*
+Translation hiahiatia.
+*/}),
+ "Marathi": HAIKU(function() {/*
+भाषांतर करणे आवश्यक आहे.
+*/}),
+ "Mongolian": HAIKU(function() {/*
+Орчуулга хэрэгтэй.
+*/}),
+ "Myanmar (Burmese)": HAIKU(function() {/*
+ဘာသာပြန်ချက်လိုအပ်ခဲ့ပါတယ်။
+*/}),
+ "Nepali": HAIKU(function() {/*
+अनुवाद आवश्यक।
+*/}),
+ "Norwegian": HAIKU(function() {/*
+Oversettelse nødvendig.
+*/}),
+ "Pashto": HAIKU(function() {/*
+ژباړه ته اړتیا لري.
+*/}),
+ "Persian": HAIKU(function() {/*
+ترجمه مورد نیاز است.
+*/}),
+ "Polish": HAIKU(function() {/*
+Tłumaczenie potrzebne.
+*/}),
+ "Portuguese": HAIKU(function() {/*
+Tradução necessário.
+*/}),
+ "Punjabi": HAIKU(function() {/*
+ਅਨੁਵਾਦ ਦੀ ਲੋੜ ਸੀ.
+*/}),
+ "Romanian": HAIKU(function() {/*
+E nevoie de traducere.
+*/}),
+ "Russian": HAIKU(function() {/*
+E nevoie de traducere.
+*/}),
+ "Samoan": HAIKU(function() {/*
+manaomia Faaliliuga.
+*/}),
+ "Scots Gaelic": HAIKU(function() {/*
+Eadar-theangachadh a dhìth.
+*/}),
+ "Serbian": HAIKU(function() {/*
+потребан превод.
+*/}),
+ "Sesotho": HAIKU(function() {/*
+Phetolelo e hlokahalang.
+*/}),
+ "Shona": HAIKU(function() {/*
+Translation inodiwa.
+*/}),
+ "Sindhi": HAIKU(function() {/*
+ترجمو جي ضرورت آهي.
+*/}),
+ "Sinhala": HAIKU(function() {/*
+පරිවර්තනය අවශ්ය විය.
+*/}),
+ "Slovak": HAIKU(function() {/*
+Preklad potreba.
+*/}),
+ "Slovenian": HAIKU(function() {/*
+Prevajanje potrebna.
+*/}),
+ "Somali": HAIKU(function() {/*
+Translation loo baahan yahay.
+*/}),
+ "Spanish": HAIKU(function() {/*
+La traducción necesaria.
+*/}),
+ "Sundanese": HAIKU(function() {/*
+Tarjamahan diperlukeun.
+*/}),
+ "Swahili": HAIKU(function() {/*
+Tafsiri inahitajika.
+*/}),
+ "Swedish": HAIKU(function() {/*
+Översättning behövs.
+*/}),
+ "Tajik": HAIKU(function() {/*
+Тарҷумаи лозим.
+*/}),
+ "Tamil": HAIKU(function() {/*
+மொழிபெயர்ப்பு தேவை.
+*/}),
+ "Telugu": HAIKU(function() {/*
+అనువాద అవసరమైన.
+*/}),
+ "Thai": HAIKU(function() {/*
+แปลจำเป็น
+*/}),
+ "Turkish": HAIKU(function() {/*
+Çeviri gerekli.
+*/}),
+ "Ukrainian": HAIKU(function() {/*
+потрібен переклад.
+*/}),
+ "Urdu": HAIKU(function() {/*
+ہے.ترجمے کی ضرورت.
+*/}),
+ "Uzbek": HAIKU(function() {/*
+Tarjima zarur.
+*/}),
+ "Vietnamese": HAIKU(function() {/*
+Dịch cần thiết.
+*/}),
+ "Welsh": HAIKU(function() {/*
+angen cyfieithiad.
+*/}),
+ "Xhosa": HAIKU(function() {/*
+Translation ezifunekayo.
+*/}),
+ "Yiddish": HAIKU(function() {/*
+דזשאַדזשינג זינד איז זינד.
+גאָט סאָרץ אונדז אויס באַלד גענוג.
+*/}),
+ "Yoruba": HAIKU(function() {/*
+Translation ti nilo.
+*/}),
+ "Zulu": HAIKU(function() {/*
+Labahumushi lalidinga abantu.
+*/}),
+};
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //(function() {
-document.haiku = {speaking:'English'};
 function display(language) {
   // This function copies a hidden haiku to the display table cell.
   var source = document.getElementById(language);
@@ -12,32 +11,40 @@ function mouseEnter(language) {                  display(language); }
 function mouseLeave(language) {   display(document.haiku.speaking); }
 function mouseClick(language) { document.haiku.speaking = language; }
 
-var haiku = $('.haiku');        // Fetch list of all hidden text
-var languages = haiku.length;   // Count the languages
-var width = 6;                  // Specify horizontal language count
+//requirejs.config({"baseUrl": ""});
+//requirejs(['haiku.js']);
+//$(document).ready(function() {
+  //var haiku = document.haiku;
+  var haiku = $('.haiku');        // Fetch list of all hidden text
+  var languages = haiku.length;   // Count the languages
+  var width = 6;                  // Specify horizontal language count
 
-// Generate the menu on-the-fly from language names (id value of haiku)
-var table = '<table align="center">';
-for (var row of _.range(0, languages, width)) {
-  // Make a new table row
-  table += '<tr>';
-  for (var col of _.range(width)) {
-    var I = row + col;
-    var language = I;
-    if (language >= languages) break;  // Quit if no more languages.
-    var element = haiku[language];
-    var id = element.id;
-    var onEnter = ' onmouseenter="mouseEnter(\'' + id + '\')"';
-    var onLeave = ' onmouseleave="mouseLeave(\'' + id + '\')"';
-    var onClick =      ' onclick="mouseClick(\'' + id + '\')"';
-    var onMouse = onEnter + onLeave + onClick;
-    table += '<td class="Language" ' + onMouse + '>' + id + '</td>';
+  document.haiku = {};
+  document.haiku.speaking = 'English';
+
+  // Generate the menu on-the-fly from language names (id value of haiku)
+  var table = '<table align="center">';
+  for (var row of _.range(0, languages, width)) {
+    // Make a new table row
+    table += '<tr>';
+    for (var col of _.range(width)) {
+      var I = row + col;
+      var language = I;
+      if (language >= languages) break;  // Quit if no more languages.
+      var element = haiku[language];
+      var id = element.id;
+      var onEnter = ' onmouseenter="mouseEnter(\'' + id + '\')"';
+      var onLeave = ' onmouseleave="mouseLeave(\'' + id + '\')"';
+      var onClick =      ' onclick="mouseClick(\'' + id + '\')"';
+      var onMouse = onEnter + onLeave + onClick;
+      table += '<td class="Language" ' + onMouse + '>' + id + '</td>';
+    }
+    table += '</tr>';
   }
-  table += '</tr>';
-}
-table += '</table>';
+  table += '</table>';
 
-$('.MENU').html(table);         // Fill the menu with the generated table
+  $('.MENU').html(table);         // Fill the menu with the generated table
 
-display(document.haiku.speaking); // Initialize display for default language.
+  display(document.haiku.speaking); // Initialize display for default language.
+//});
 //})();
